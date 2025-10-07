70c0c73dbcb64508b2066f2b9bf2e82bc0caa72c
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2025-10-07 17:44:07 +0300

    realestate photos var fix

--- ShopTARgv24/Models/RealEstate/RealEstateDeleteViewModel.cs
+++ ShopTARgv24/Models/RealEstate/RealEstateDeleteViewModel.cs
@@ -8,6 +8,7 @@
         public int? RoomNumber { get; set; }
         public string? BuildingType { get; set; }
 
+
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
     }
