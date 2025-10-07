28b42aa9768f9a4a00e992954c8de08c848c0c35
Gleb Dranitsyn <GlebDranitsyn@hotmail.com>
2025-10-07 14:10:22 +0300

    Добавьте файлы проекта.

---
+++ ShopTARgv24.ApplicationServices/Services/RealEstateServices.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARgv24.Core.Domain;
+using ShopTARgv24.Core.Dto;
+using ShopTARgv24.Core.ServiceInterface;
+using ShopTARgv24.Data;
+
+
+namespace ShopTARgv24.ApplicationServices.Services
+{
+    public class RealEstateServices : IRealEstateServices
+    {
+        private readonly ShopTARgv24Context _context;
+        private readonly IFileServices _fileServices;
+
+        public RealEstateServices
+            (
+                ShopTARgv24Context context,
+                IFileServices fileServices
+            )
+        {
+            _context = context;
+            _fileServices = fileServices;
+        }
+
+        public async Task<RealEstate> Create(RealEstateDto dto)
+        {
+            RealEstate domain = new RealEstate();
+
+            domain.Id = dto.Id;
+            domain.Area = dto.Area;
+            domain.Location = dto.Location;
+            domain.RoomNumber = dto.RoomNumber;
+            domain.BuildingType = dto.BuildingType;
+            domain.CreatedAt = DateTime.Now;
+            domain.ModifiedAt = DateTime.Now;
+
+            //peaks kontrollima, kas on faile v]i ei ole
+            if (dto.Files != null)
+            {
+                _fileServices.UploadFilesToDatabase(dto, domain);
+            }
+
+            await _context.RealEstate.AddAsync(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+
+        public async Task<RealEstate> Update(RealEstateDto dto)
+        {
+            RealEstate domain = new RealEstate();
+
+            domain.Id = dto.Id;
+            domain.Area = dto.Area;
+            domain.Location = dto.Location;
+            domain.RoomNumber = dto.RoomNumber;
+            domain.BuildingType = dto.BuildingType;
+            domain.CreatedAt = dto.CreatedAt;
+            domain.ModifiedAt = DateTime.Now;
+
+            _context.RealEstate.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+
+        public async Task<RealEstate> DetailAsync(Guid id)
+        {
+            var result = await _context.RealEstate
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+        public async Task<RealEstate> Delete(Guid id)
+        {
+            var result = await _context.RealEstate
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            _context.RealEstate.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return result;
+        }
+
+    }
+}
