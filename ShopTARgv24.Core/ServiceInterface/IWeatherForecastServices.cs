415fe2f38d44dd972c520062d610e4d18f817408
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2025-10-13 17:26:06 +0300

    controller

--- ShopTARgv24.Core/ServiceInterface/IWeatherForecastServices.cs
+++ ShopTARgv24.Core/ServiceInterface/IWeatherForecastServices.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShopTARgv24.Core.Dto;
 
 namespace ShopTARgv24.Core.ServiceInterface
 {
-    internal interface IWeatherForecastServices
+    public interface IWeatherForecastServices
     {
+        Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto);
     }
 }
--- ShopTARgv24/Controllers/WeatherController.cs
+++ ShopTARgv24/Controllers/WeatherController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ShopTARgv24.ApplicationServices.Services;
+using ShopTARgv24.Core.ServiceInterface;
 
 namespace ShopTARgv24.Controllers
 {
     public class WeatherController : Controller
     {
+        public WeatherController
+            (
+            IWeatherForecastServices weatherForecastServices
+            )
+        {
+            _weatherForecastServices = weatherForecastServices;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
     }
 }
