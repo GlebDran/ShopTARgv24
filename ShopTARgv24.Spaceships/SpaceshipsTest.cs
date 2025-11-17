11a9fc745d43249ec6b12481be1d1be0778b9b34
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2025-11-17 17:22:22 +0200

    upd

--- ShopTARgv24.Spaceships/SpaceshipsTest.cs
+++ ShopTARgv24.Spaceships/SpaceshipsTest.cs
@@ -1,7 +1,7 @@
 ﻿using System.Globalization;
 using ShopTARgv24.Core.Dto;
 using ShopTARgv24.Core.ServiceInterface;
-using ShopTARgv24.RealEstateTest;
+using ShopTARgv24.SpaceShipsTest;
 
 namespace ShopTARgv24.SpaceShipsTest
 {
