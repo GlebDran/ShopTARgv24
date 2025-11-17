611744a9625228326a62da8b572039444e419b35
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2025-11-17 17:27:22 +0200

    upd

--- ShopTARgv24.Core/ServiceInterface/ICocktailService.cs
+++ ShopTARgv24.Core/ServiceInterface/ICocktailService.cs
@@ -11,13 +11,13 @@ namespace ShopTARgv24.Core.ServiceInterface
 
     public class CocktailDto
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Thumb { get; set; }
-        public string Category { get; set; }
-        public string Alcoholic { get; set; }
-        public string Glass { get; set; }
-        public string Instructions { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Thumb { get; set; }
+        public string? Category { get; set; }
+        public string? Alcoholic { get; set; }
+        public string? Glass { get; set; }
+        public string? Instructions { get; set; }
         public List<string> Ingredients { get; set; } = new();
     }
 }
